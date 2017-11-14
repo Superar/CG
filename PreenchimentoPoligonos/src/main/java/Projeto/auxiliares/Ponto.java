3a43e59c5edbeb8232a6da538bfdace638eca936
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-11-14 15:50:32 -0200

    Correção na rotação dos sólidos e integração com o teclado.

--- PreenchimentoPoligonos/src/main/java/Projeto/auxiliares/Ponto.java
+++ PreenchimentoPoligonos/src/main/java/Projeto/auxiliares/Ponto.java
@@ -7,7 +7,7 @@ public class Ponto {
     private int x, y, z;
 
     private INDArray matrizProjecao;
-    private static final float FOCALD = -900;
+    private static final float FOCALD = -1000;
 
     public Ponto(int x, int y, int z) {
         this.x = x;
@@ -41,6 +41,21 @@ public class Ponto {
         return new Ponto(pontoProjetado.getInt(0, 0),
                          pontoProjetado.getInt(1, 0),
                          pontoProjetado.getInt(2, 0));
+    }
+
+    public Ponto rotacionaPonto(float angulo, float x, float y, float z) {
+        float cos = (float) Math.cos(angulo);
+        float sen = (float) Math.sin(angulo);
+        INDArray matrizRotacao = Nd4j.create(new float[]{ x*x*(1-cos)+cos, x*y*(1-cos)-z*sen, x*z*(1-cos)+y*sen, 0,
+                                                          y*x*(1-cos)+z*sen, y*y*(1-cos)+cos, y*z*(1-cos)-x*sen, 0,
+                                                          x*z*(1-cos)-y*sen, y*z*(1-cos)+x*sen, z*z*(1-cos)+cos, 0,
+                                                          0, 0, 0, 1},
+                                             new int[]{4, 4});
+        INDArray ponto = Nd4j.create(new float[]{this.x, this.y, this.z, 1}, new int[]{4, 1});
+        INDArray pontoRotacionado = matrizRotacao.mmul(ponto);
+        return new Ponto(pontoRotacionado.getInt(0, 0),
+                         pontoRotacionado.getInt(1, 0),
+                         pontoRotacionado.getInt(2, 0));
     }
 
     @Override
